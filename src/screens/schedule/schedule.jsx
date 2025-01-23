6d6a0219bdf71461f44db25296f43b50ebfd5b3e
Jefferson Batista <jef9296@gmail.com>
2025-01-23 17:40:21 -0300

    Finalizado a tela de Confirmar Reserva

--- src/screens/schedule/schedule.jsx
+++ src/screens/schedule/schedule.jsx
@@ -3,28 +3,53 @@ import { styles } from "./schedule.style"
 import { Calendar, LocaleConfig } from "react-native-calendars";
 import { ptBR } from "../../constants/calendar";
 import { useState } from "react";
+import { Picker } from "@react-native-picker/picker";
+import Button from "../../components/button/button";
 
 LocaleConfig.locales["pt-br"] = ptBR;   //Traduzido a linguagem do calendario
 LocaleConfig.defaultLocale = "pt-br";
 
 function Schedule() {
 
-    //Projeto calendário:
-    //https://github.com/wix/react-native-calendars
+    //Proj calendário: https://github.com/wix/react-native-calendars
+    //Proj picker: https://github.com/react-native-picker/picker
 
-    const [selected, setSelected] = useState("");
+    const [selectedDate, setSelectedDate] = useState(new Date().toISOString().slice(0, 10));
+    const [selectedHour, setSelectedHour] = useState("");
 
     return <View style={styles.container}>
-        <Calendar theme={styles.theme}
-            onDayPress={(day) => {
-                setSelected(day.dateString)
-            }}
-            markedDates={{
-                [selected]: { selected: true }
-            }}
-            //Elegendo a data minima para marcação de dia
-            minDate={new Date().toDateString()}
-        />
+        <View>
+            <Calendar theme={styles.theme}
+                onDayPress={(day) => {
+                    setSelectedDate(day.dateString)
+                }}
+                markedDates={{
+                    [selectedDate]: { selected: true }
+                }}
+                //Elegendo a data minima para marcação de dia
+                minDate={new Date().toDateString()}
+            />
+
+            <View>
+                <Text style={styles.textHour}>Horário</Text>
+            </View>
+
+            <View>
+                <Picker selectedValue={selectedHour}
+                    onValueChange={(itemValue, itemIndex) => {
+                        setSelectedHour(itemValue)
+                    }}>
+                    <Picker.Item label="09:00" value="09:00" />
+                    <Picker.Item label="09:30" value="09:30" />
+                    <Picker.Item label="09:40" value="09:40" />
+                </Picker>
+            </View>
+
+        </View>
+
+        <View>
+            <Button text="Confirmar Reserva" />
+        </View>
     </View>
 }
 
--- src/screens/schedule/schedule.style.js
+++ src/screens/schedule/schedule.style.js
@@ -5,12 +5,20 @@ export const styles = {
         flex: 1,
         backgroundColor: COLORS.white,
         paddingLeft: 20,
-        paddingRight: 20
+        paddingRight: 20,
+        justifyContent:"space-between",
+        marginBottom:20
     },
     theme: {
         todayTextColor: COLORS.red,
         selectedDayBackgroundColor: COLORS.blue,
         selectedDayTextColor: COLORS.white,
         arrowColor: COLORS.blue
+    },
+    textHour:{
+        fontSize:FONT_SIZE.lg,
+        fontWeight:"bold",
+        color:COLORS.gray2,
+        marginTop:20
     }
 }
